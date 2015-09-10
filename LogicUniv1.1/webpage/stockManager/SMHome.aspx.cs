8467299b53949a386d4e6897efac7768b8aa729e
pengxiaomeng <pxmlw@126.com>
2015-09-10 19:17:43 +0800

    test

--- LogicUniv1.1/webpage/stockManager/SMHome.aspx.cs
+++ LogicUniv1.1/webpage/stockManager/SMHome.aspx.cs
@@ -8,35 +8,32 @@ using ClassLibraryBL.EntityFacade;
 using ClassLibraryBL.controller;
 using ClassLibraryBL.Entities;
 using ClassLibraryBL;
-
+using ClassLibraryBL.Controller.stockManager;
 
 namespace LogicUniv1._1.webpage.stockManager
 {
     public partial class SMHome : System.Web.UI.Page
     {
-        LogicUnivSystemEntities ctx = new LogicUnivSystemEntities();
+        ApproveStockAdjustmentController Manager = new ApproveStockAdjustmentController();
         protected void Page_Load(object sender, EventArgs e)
         {
-                User userbean = (User)Session["UserEntity"];
-                storeM.Text = "Hello," + userbean.Name;
-                var N = from a in ctx.discrepancies
-                        join b in ctx.users on a.userId equals b.userId
-                        where a.status == "Pending"
-                        select new
-                        {
-                            DiscrepancyId = a.discrepancyId,
-                            Clerk = b.name,
-                            Date = a.reportDate,
-                            Remark = a.Remark,
-                            TotalPrice = a.totalPrice,
-                            Status = a.status
-                        };
-               
-                GridView1.DataSource = N.ToList();
-                GridView1.DataBind();
-               
-               
-                
+                //User userbean = (User)Session["UserEntity"];
+                //storeM.Text = "Hello," + userbean.Name;
+                //var N = from a in ctx.discrepancies
+                //        join b in ctx.users on a.userId equals b.userId
+                //        where a.status == "Pending"
+                //        select new
+                //        {
+                //            DiscrepancyId = a.discrepancyId,
+                //            Clerk = b.name,
+                //            Date = a.reportDate,
+                //            Remark = a.Remark,
+                //            TotalPrice = a.totalPrice,
+                //            Status = a.status
+                //        };
+
+                GridView1.DataSource = Manager.checkdiscrepancy();
+                GridView1.DataBind();                
             
         }
         protected void GridView1_PageIndexChanging1(object sender, GridViewPageEventArgs e)
